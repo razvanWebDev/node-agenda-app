41c67359383d3a8307af3cb02b6f51784a814791
Crisan Razvan <crsn_razvan@yahoo.com>
2019-01-04 13:31:23 +0200

    created table in json

---
+++ public/index.js
@@ -0,0 +1,19 @@
+function displayAgenda(agenda) {
+    var resultTable = document.querySelector('#contacts table');
+
+    var contacts = agenda.map(function (agenda) {
+        return `<tr>
+                    <td>${agenda.firstName}</td> <td>${agenda.lastName}</td> <td>${agenda.phoneNr}</td>
+                </tr>`
+    })
+    resultTable.innerHTML = contacts.join("");
+}
+
+function initAgenda() {
+    $.ajax('agenda.json').done(function (agenda) {
+        displayAgenda(agenda);
+    })
+}
+
+
+initAgenda();
