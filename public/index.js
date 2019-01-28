1be08aa116bf799c27d254ed4b850d3a7f10f443
Crisan Razvan <crsn_razvan@yahoo.com>
2019-01-28 13:08:15 +0200

    fixed contact modify bug

--- public/index.js
+++ public/index.js
@@ -1,73 +1,84 @@
 var phoneToEdit = '';
 
-function getNewRow() {
-    return `<tr>
-                    <td><input type="text" name="firstName" placeholder="first Name"/></td>
-                    <td><input type="text" name="lastName" placeholder="last Name"/></td>
-                    <td><input type="text" name="phone" placeholder="phone"/></td>
-                    <td><button onclick="saveContact()">Save</button></td>
-                </tr>`
-}
-
-function saveContact(){
- var firstName = $('input[name=firstName]').val();
- var lastName = $('input[name=lastName]').val();
- var phone = document.querySelector('input[name=phone]').value;
-
- console.log('save contact', firstName, lastName, phone);
-
-    var actionUrl = phoneToEdit ? 'contacts/update?phone= ' + phoneToEdit : "contacts/create";
- 
- $.post(actionUrl, {
-     firstName, //shortcut from ES6 (key is the same as value variable name)
-     lastName: lastName,
-     phone: phone
- }).done(function(response){
-     console.warn('done create contact');
-     if (response.success){
-         loadContacts();
-     }
- })
-}
-
 function loadContacts() {
-    $.ajax('contacts.json').done(function (contacts) {
+    $.ajax('data/contacts.json').done(function(contacts){
+        console.info('contacts loaded', contacts);
         window.globalContacts = contacts;
         displayContacts(contacts);
-    })
+    });
+}
+
+function getNewRow() {
+    return `<tr>
+            <td><input type="text" name="firstName" placeholder="First Name"/></td>
+            <td><input type="text" name="lastName" placeholder="Last Name"/></td>
+            <td><input type="text" name="phone" placeholder="Phone"/></td>
+            <td><button onclick="saveContact()">Save</button></td>
+        </tr>`;
+}
+
+function saveContact() {
+    var firstName = document.querySelector('input[name=firstName]').value;
+    var lastName = $('input[name=lastName]').val();
+    var phone = $('input[name=phone]').val();
+    console.debug('saveContact...', firstName, lastName, phone);
+    
+    // var actionUrl = '';
+    // if (phoneToEdit){ actionUrl = 'contacts/update' } else {actionUrl = 'contacts/create'}
+    var actionUrl = phoneToEdit ? 'contacts/update?phone=' + phoneToEdit : 'contacts/create';
+    
+    $.post(actionUrl, {
+        firstName, // shortcut from ES6 (key is the same as value variable name)
+        lastName,
+        phone: phone // ES5 (key = value)
+    }).done(function(response){
+        console.warn('done create contact', response);
+        phoneToEdit = '';
+        if (response.success) {
+            loadContacts();
+        }
+    });
 }
 
 function displayContacts(contacts) {
-    var resultTable = document.querySelector('#agenda tbody');
-
-    var rows = contacts.map(function (contact) {
+    var rows = contacts.map(function(contact) {
+        console.log('transform contact', contact);
         return `<tr>
-                    <td>${contact.firstName}</td>
-                    <td>${contact.lastName}</td>
-                    <td>${contact.phone}</td>
-                    <td>
-                        <a href="/contacts/delete?phone=${contact.phone}">&#10006</a>
-                        <a href = "#" class="edit" data-id="${contact.phone}" >&#9998</a>
-                    </td>
-                </tr>`
+            <td>${contact.firstName}</td>
+            <td>${contact.lastName}</td>
+            <td>${contact.phone}</td>
+            <td>
+                <a href="/contacts/delete?phone=${contact.phone}">&#10006;</a>
+                <a href="#" class="edit" data-id="${contact.phone}">&#9998;</a>
+            </td>
+        </tr>`;
     });
-    rows.push(getNewRow());
-    resultTable.innerHTML = rows.join("");
+    console.warn('rows', rows);
+    
+    //rows.push(getNewRow()); // simplified
+    var actions = getNewRow();
+    rows.push(actions);
+
+    document.querySelector('tbody').innerHTML = rows.join('');
 }
 
-function initEvents(){
-    $( "tbody" ).delegate( "a.edit", "click", function() {
+function initEvents() {
+    // TODO use native click
+    $("tbody").delegate( "a.edit", "click", function() {
         phoneToEdit = this.getAttribute('data-id');
+
         var contact = globalContacts.find(function(contact){
             return contact.phone == phoneToEdit;
-            })
-        $('input[name=firstName]').val(contact.firstName);
-        $('input[name=lastName]').val(contact.lastName);
-        document.querySelector('input[name=phone]').value = contact.phone;
-
+        });
+        console.log('edit', phoneToEdit, contact);
         
-      });
+        document.querySelector('input[name=firstName]').value = contact.firstName;
+        $('input[name=lastName]').val(contact.lastName);
+        $('input[name=phone]').val(contact.phone);
+    });
 }
+
+// - start app
 
 loadContacts();
 initEvents();
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,75 +7,69 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-
 // /contacts/delete?phone=1234
 router.get('/delete', function(req, res, next) {
   var phone = req.query.phone;
-  var content = fs.readFileSync('public/contacts.json');
+
+  var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
- 
 
   var remainingContacts = contacts.filter(function(contact){
     return contact.phone != phone;
   });
 
- content = JSON.stringify(remainingContacts, null, 2);
-  fs.writeFileSync('public/contacts.json' , content);
-
-  // res.json({success: true});
-
-  res.redirect('/agenda.html');
+  content = JSON.stringify(remainingContacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
   
+  //res.json({success: true});
+  // TODO please redirect to agenda.html
+  res.redirect('/agenda.html');
 });
 
-
-
-
-// /contacts Create
+// /contacts/create
 router.post('/create', function(req, res, next) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
 
-  var content = fs.readFileSync('public/contacts.json');
+  var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
- 
+
   contacts.push({
-    firstName, 
-    lastName,
-    phone
-});
+      firstName,
+      lastName,
+      phone
+  });
+
+  content = JSON.stringify(contacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
   
-
- content = JSON.stringify(contacts, null, 2);
-  fs.writeFileSync('public/contacts.json' , content);
-
-  res.json({success: true});  
+  res.json({success: true});
 });
 
-
-// /contacts update
+// /contacts/update
 router.post('/update', function(req, res, next) {
-  var oldPhone = req.query.phone;
+  var oldPhone = req.query.phone; // TODO id
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
 
-  var content = fs.readFileSync('public/contacts.json');
+  var content = fs.readFileSync('public/data/contacts.json');
   var contacts = JSON.parse(content);
- //update
- var contact = contacts.finnd(function(contact,){
-  return contact.phone == oldPhone;
- });
-contact.firstName = firstName;
-contact.lastName = lastName;
- contact.phone = phone;
 
- content = JSON.stringify(contacts, null, 2);
-  fs.writeFileSync('public/contacts.json' , content);
+  // update... (v1)
+  var contact = contacts.find(function(contact){
+    return contact.phone == oldPhone;
+  });
+  contact.firstName = firstName;
+  contact.lastName = lastName;
+  contact.phone = phone;
+  // TODO update... (v2);
 
-  res.json({success: true});  
+  content = JSON.stringify(contacts, null, 2);
+  fs.writeFileSync('public/data/contacts.json', content);
+  
+  res.json({success: true});
 });
-
 
 module.exports = router;
